a130294e0da2fce2763cfb88cd0f63adc66f3f5d
Hack3r-han <hannahphil27@gmail.com>
2023-11-28 12:53:43 +0100

    Change the Square's properties and repeat it

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function change() {
-    
+    // let colorBlue = document.querySelector('.redSquare').style.backgroundColor = "blue";
+    // let shapeRectangle = document.querySelector('.redSquare').style.width = "200px";
+
+    let div = document.createElement("div");
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.background = "red";
+    div.style.margin = ".5%";
+
+    document.querySelector(".redSquare").appendChild(div);
 }
